c54a6f711f16eacac23282e117642e374a919e8a
Rabbanihosni10 <rabbanihosni10@gmail.com>
2025-10-16 05:39:34 +0600

    12

--- dart_concept/datatype_practice.dart
+++ dart_concept/datatype_practice.dart
@@ -94,4 +94,92 @@ void main() {
   print("$upp");
   // String isFirstName = name
   // print(isFirstName)
+  String firstName = "Hosni";
+  if (upp.contains(firstName)) {
+    print(true);
+  } else {
+    print(false);
+  }
+
+  // String tobarok1 = firstName.substring();
+  int index_space = namu.indexOf(' ');
+  String first_name = namu.substring(0, index_space);
+  String last_name = namu.substring(index_space + 1, lengu);
+  print(first_name);
+  print(last_name);
+
+  /**
+   * ### Question 6: Boolean Logic
+Declare two boolean variables:
+- `isRaining = true`
+- `hasUmbrella = false`
+
+Write an expression that determines: "Should I go outside?" (true if it's not raining OR I have an umbrella)
+   */
+  bool isRaining = true;
+  bool hasUmbrella = false;
+  bool flag_result = true;
+  if (isRaining == true && hasUmbrella == false) {
+    flag_result = false;
+  }
+  print("Should I go outside?");
+  if (flag_result == false) {
+    print("No");
+  } else {
+    print("Yes");
+  }
+
+  /**
+   * Question 8: Sets
+Create a Set of String containing names of 3 fruits. Then:
+- Try adding a duplicate fruit name
+- Check if "apple" exists in the set
+- What's the difference between a List and a Set?
+  */
+  Set fruta = {'Banana', 'Apple', 'Pineaple'};
+  fruta.add('Banana');
+  print(fruta);
+
+  //in sets duplicate value cant be entered again
+
+  print(fruta.contains('Apple'));
+  print(fruta.contains('apple'));
+
+  //   # Question 9: Maps
+  // Create a Map where:
+  // - Keys are student names (String)
+  // - Values are their ages (int)
+
+  Map murtada = {1: 'Banana', 2: 'Apple', 3: 'Pineaple'};
+  Map stu_nam_age = {'Roksana': 12, 'Tobarok': 23, 'polapain': 30};
+  print(stu_nam_age);
+  /**
+   * Add at least 3 students, then:
+- Print a specific student's age
+- Update one student's age
+- Add a new student
+   */
+  //map worked like dictionary
+
+  stu_nam_age['Rubel'] = 23;
+  stu_nam_age['Nehal'] = 32;
+  stu_nam_age['Khairul'] = 50;
+
+  print(stu_nam_age);
+  stu_nam_age['Rubel'] = 45;
+  print(stu_nam_age);
+  stu_nam_age['Maria'] = 11;
+  print(stu_nam_age);
+
+  /**
+   * Question 10: Dynamic vs Object
+What's the difference between these declarations?
+```dart
+dynamic x = 10;
+Object y = 10;
+var z = 10;
+```
+Can you reassign different types to each?
+
+   */
 }
